0c8d9f833c884af610d64ab75a2a0b0f933fdc77
Andrij Vasylkiv <andrijvasilkiv7@gmail.com>
2023-03-06 13:11:17 +0200

    fetch start

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const f =
+    fetch("https://restcountries.com/v3.1/all")
+        .then(response => {
+           
+            return response.json();
+        })
+        .then(country => {
+            console.log(country)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+
+// console.log(f)
+
+
+
+// fetchCountries(name)
