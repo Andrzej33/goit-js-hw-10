c94317ef86ae800c44b0e03e2e33dc0fe1b16bfb
Andrij Vasylkiv <andrijvasilkiv7@gmail.com>
2023-03-09 13:19:11 +0200

    hw done

--- src/fecthCountries.js
+++ src/fecthCountries.js
@@ -1,42 +1,19 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-// export default { fetchCountries };
-
-
-// export const fetchCountries = name => {
-//     return fetch("https://restcountries.com/v2/name/${name}")
-//         .then(response => {
-//             if (!response.ok) {
-//                 if (response.status === 404) {
-//                     return [];
-//                 }
-//                 throw new Error(response.status);
-//             }
-//             return response.json();
-//         })
-// }
-
-// ?fields=name,capital,population,flags,languages
-
-
-// function makeCountryList(data) {
-//     return data.reduce((acum, country) => acum += `<li>${country.capital}</li>`,''
-//     )   
-// }
-
-// const countries = [
-//     {
-//         capital: 'Peu',
-//         population: 2000,
-//     },
-//     {
-//         capital: 'moca',
-// population:4000,    },
-//     {
-//         capital: 'ternau',
-//     population: 500,}
-
-// ]
-// console.log('hello')
- 
-
-
+export function fetchCountries(name) {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+  ).then(response => {
+    if (!response.ok) {
+      if (response.status === 404) {
+        Notify.failure('Oops, there is no country with that name', {
+          showOnlyTheLastOne: true,
+          clickToClose: true,
+        });
+        return [];
+      }
+      throw new Error(responce.status);
+    }
+    return response.json();
+  });
+}
--- src/index.js
+++ src/index.js
@@ -1,112 +1,74 @@
 import './css/styles.css';
-// import { fetchCountries } from './fecthCountries';
+import { fetchCountries } from './fecthCountries';
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const DEBOUNCE_DELAY = 300;
 
-
-
 const refs = {
-    input: document.getElementById('search-box'),
-    countries: document.querySelector('.country-list'),
-    country: document.querySelector('.country-info'),
-}
+  input: document.getElementById('search-box'),
+  countries: document.querySelector('.country-list'),
+  country: document.querySelector('.country-info'),
+};
 
-refs.input.addEventListener('input', debounce(onCountryElected,DEBOUNCE_DELAY))
+refs.input.addEventListener('input', debounce(onCountryInput, DEBOUNCE_DELAY));
 
-function onCountryElected(evt) {
-    evt.preventDefault();
-    const name = refs.input.value.trim();
-    clearPage();
-    if (name !== '') {
-        fetchCountries(name)
-            .then(countries => {
-                if (countries.length === 1) {
-                    createSingleCountry(countries)
-                } else if
-                    (countries.length >= 2 && countries.length <= 10) {
-                    makeCountryList(countries)
-                }
-           else if
-                    (countries.length > 10) {
-                    Notify.info('Too many matches found. Please enter a more specific name.');
-                }
+function onCountryInput(evt) {
+  evt.preventDefault();
+  const name = refs.input.value.trim();
+  clearPage();
+  if (name !== '') {
+    fetchCountries(name)
+      .then(countries => {
+        if (countries.length === 1) {
+          createSingleCountry(countries);
+        } else if (countries.length >= 2 && countries.length <= 10) {
+          makeCountryList(countries);
+        } else if (countries.length > 10) {
+          Notify.info(
+            'Too many matches found. Please enter a more specific name.',
+            {
+              showOnlyTheLastOne: true,
+              clickToClose: true,
             }
-    
-            )
-        // .catch(error => {
-        //     countries = [];
-        //     console.log(error.message)
-        // })
-        
-    }
+          );
+        }
+      })
+      .catch(error => {
+        countries = [];
+        console.log(error.message);
+      });
+  }
 }
-
-// console.log(refs.input)
-
-// function catchError(error) {
-// console.log(error)
-//             alert('choose more specyfic country')
-// }
-
-
-
-
-// console.log(f)
-
-
-
-// fetchCountries(name)
-function fetchCountries(name) {
-    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
-        .then(response => {
-            if (!response.ok) {
-                if (response.status === 404) {
-                    Notify.failure("Oops, there is no country with that name")
-                    return []
-                }
-                throw new Error(responce.status);
-            }
-            return response.json();
-        })
-}
-       
-
-
-// fetchCountries()
-
-// function makeCountryList(data) {
-//     const markap = data.reduce((acum, country) => {
-//         return }, ''
-
-
-// console.log(makeCountryList(countries))
-
-        
 
 function makeCountryList(data) {
-    const markup = data.reduce((acum, country) => 
-     acum += `<li class=country-item>
-             <img src="${country.flags.svg}" alt="${country.name.common} flag" width="30" height="30" />
+  const markup = data.reduce(
+    (acum, country) =>
+      (acum += `<li class=country-item>
+             <img src="${country.flags.svg}" alt="${country.name.common} flag" width="30" height="20" />
             <p>${country.name.official}</p>
-         </li>`, '');
-    refs.countries.innerHTML = markup
+         </li>`),
+    ''
+  );
+  refs.countries.innerHTML = markup;
 }
-        
+
 function createSingleCountry(data) {
-const markup = data.reduce((acum, country) => 
-     acum += `
-    <img src="${country.flags.svg}" alt="${country.name.common} flag" width="40" height="30">
+  const markup = data.reduce(
+    (acum, country) =>
+      (acum += `
+    <img src="${country.flags.svg}" alt="${
+        country.name.common
+      } flag" width="40" height="30">
     <p>${country.name.official}</p>
     <p>Capital: ${country.capital}</p>
     <p>Population: ${country.population}</p>
-    <p>Language: ${country.languages}</p>`, '');
-    refs.country.innerHTML = markup
+    <p>Language: ${Object.values(country.languages)}</p>`),
+    ''
+  );
+  refs.country.innerHTML = markup;
 }
 
-
-
 function clearPage() {
-    refs.country.innerHTML = '';
-    refs.countries.innerHTML = '';
+  refs.country.innerHTML = '';
+  refs.countries.innerHTML = '';
 }
